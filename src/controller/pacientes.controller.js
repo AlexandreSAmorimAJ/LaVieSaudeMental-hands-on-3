420c89e33b1f313ef7328c4bbf1ff0e3de61ec21
GuilhermeRibeiro01 <109393052+GuilhermeRibeiro01@users.noreply.github.com>
2022-09-14 18:26:57 -0300

    Update pacientes.controller.js

--- src/controller/pacientes.controller.js
+++ src/controller/pacientes.controller.js
@@ -2,53 +2,73 @@ const { Pacientes } = require("../model");
 
 const PacientesController  = {
     insert: async (req,res) => {
-        const { nome, email, idade } = req.body;
-        const newPaciente = await Pacientes.create ({
-            nome,
-            email,
-            idade
-        });
-        return res.status(201).json(newPaciente)
-    },
-    listAll: async (req,res) => {
-        const pacientes = await Pacientes.findAll();
-        return res.json(pacientes);
-    }, 
-    getOne: async (req,res)  => {
-        const {id} = req.params;
-    
-        const paciente = await Pacientes.findByPk(id);
-        return res.json(paciente);
-        },
-    async attPaciente (req, res){
-        const { id } = req.params;
-        const { nome, email, idade } = req.body;
-
-        const pacienteAtualizado = await Pacientes.update(
-            {
+        try{
+            const { nome, email, idade } = req.body;
+            const newPaciente = await Pacientes.create ({
                 nome,
                 email,
-                idade,
-            },
-            {
-                where: {
-                id_paciente:id,
+                idade
+            });
+            return res.status(201).json(newPaciente);
+        }catch{
+            return res.status(400);
+        }
+    },
+    listAll: async (req,res) => {
+        try{
+            const pacientes = await Pacientes.findAll();
+            return res.json(pacientes);
+        }catch{
+            res.status(200).json();
+        }
+    }, 
+    getOne: async (req,res)  => {
+        try{
+            const {id} = req.params;
+    
+            const paciente = await Pacientes.findByPk(id);
+            return res.status(200).json(paciente);
+        }catch{
+            return res.status(404).json("Id não encontrado");
+        }
+        },
+    async attPaciente (req, res){
+        try{
+            const { id } = req.params;
+            const { nome, email, idade } = req.body;
+
+            const pacienteAtualizado = await Pacientes.update(
+                {
+                    nome,
+                    email,
+                    idade,
                 },
-            }
-        );
-        res.status(200).json("Atualizado com sucesso");
+                {
+                    where: {
+                    id_paciente:id,
+                    },
+                }
+            );
+            res.status(200).json("Atualizado com sucesso");
+        }catch{
+            res.status(400);
+        }
     },
     async deletePaciente(req, res) {
-        const { id } = req.params;
+        try{
+            const { id } = req.params;
     
-        await Pacientes.destroy({
-          where: {
+            await Pacientes.destroy({
+            where: {
             id_paciente:id,
-          },
-        });
+            },
+            });
     
-        return res.status(204);
-      },
+            return res.status(204);
+        }catch{
+            return res.status();
+        }
+    },
 }
 
 module.exports = PacientesController;
