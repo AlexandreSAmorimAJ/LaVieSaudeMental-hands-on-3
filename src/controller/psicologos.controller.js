f9ca14b6326ff2c352955d7c4221de6d4fda4de4
Eduardo Petersen <eduardu_petersen@hotmail.com>
2022-09-14 13:07:57 -0300

    update CRUD psicologos

--- src/controller/psicologos.controller.js
+++ src/controller/psicologos.controller.js
@@ -1,4 +1,4 @@
-
+const { response } = require("express");
 const psicologosModel = require("../model/psicologos");
 
 
@@ -10,17 +10,56 @@ const psicologosController = {
         return res.json(psicologo);
     },
 
-    async postOne(req, res) {
-        const { psicologos } = req.body;
-        
+    listOne: async (req, res) => {
+        const { id_psicologo } = req.params;
+
+        const onePsicologo = await psicologosModel.findByPk(id_psicologo);
+
+        res.json(onePsicologo);
+    },
+
+    createPsicologo: async (req, res) => {
+        const { nome, email,senha, apresentacao } = req.body;
+
         const newPsicologo = await psicologosModel.create({
-            nome,
-            email,
-            apresentacao
+            nome, 
+            email, 
+            senha,
+            apresentacao,
         });
 
         res.json(newPsicologo);
     },
-}
+
+    deleteOne: async (req, res) => {
+        const { id_psicologo } = req.params;
+
+        await psicologosModel.destroy({
+            where:{
+                id_psicologo,
+            },    
+        });
+
+        res.json("ID deletado com sucesso");
+    },
+
+    editOne: async(req, res) => {
+        const { id_psicologo } = req.params;
+        const { nome, email, senha, apresentacao } = req.body;
+
+        const psicologoUpdated = await psicologosModel.update(
+            {
+                nome, email, senha, apresentacao 
+            },
+            {
+                where: {
+                    id_psicologo,
+                },
+            }
+        );
+
+        res.json("Cadastro Atualizado");
+    },   
+};
 
 module.exports = psicologosController;
