6b911275b0ecc418bf8478a25d51b597d972c455
Eduardo Petersen <eduardu_petersen@hotmail.com>
2022-09-15 19:07:37 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const { hasConection } = require("./src/database");
 const routes = require("./src/routes");
-
+const handleError = require("./src/middlewares/handleError");
 const server = express();
-hasConection();
-server.use(express.json());
 
+hasConection();
+
+server.use(express.json());
 server.use(routes);
+server.use(handleError);
 
 server.listen(8000, () => console.log("Servidor rodando na porta 8000"));
