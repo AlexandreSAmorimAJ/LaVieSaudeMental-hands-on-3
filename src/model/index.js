6765c6bfe03d7d813077769ea2e905008cf684df
Eduardo Petersen <eduardu_petersen@hotmail.com>
2022-09-15 09:25:23 -0300

    Index.js

---
+++ src/model/index.js
@@ -0,0 +1,25 @@
+const Psicologos = require("./psicologos");
+const Atendimentos = require("./atendimentos");
+const Pacientes = require("./pacientes");
+
+Atendimentos.belongsTo(Pacientes, {
+    foreignKey: "id_paciente"
+});
+
+Pacientes.hasMany(Atendimentos,{
+    foreignKey: "id_paciente"
+});
+
+Atendimentos.belongsTo(Psicologos, {
+    foreignKey: "id_psicologo"
+});
+
+Psicologos.hasMany(Atendimentos, {
+    foreignKey: "id_psicologo"
+});
+
+module.exports = {
+    Psicologos,
+    Atendimentos,
+    Pacientes
+};
