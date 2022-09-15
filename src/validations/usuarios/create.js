d041e27a479a0aa2d1519bf648597acc3209b4b7
Eduardo Petersen <eduardu_petersen@hotmail.com>
2022-09-15 19:06:25 -0300

    create validations

---
+++ src/validations/usuarios/create.js
@@ -0,0 +1,10 @@
+const { validate, Joi } = require("express-validation");
+
+
+module.exports = validate({
+    body: Joi.object({
+        email: Joi.string().email().required,
+        senha: Joi.string().min(8).required()
+    }),
+
+});
