57315ce54d3be49969dfeba549d39b34df041759
Eduardo Petersen <eduardu_petersen@hotmail.com>
2022-09-15 11:10:34 -0300

    Update psicologos.controller.js

--- src/controller/psicologos.controller.js
+++ src/controller/psicologos.controller.js
@@ -1,48 +1,106 @@
 const { response } = require("express");
 const psicologosModel = require("../model/psicologos");
+
+
+
 const psicologosController = {
     listAll: async (req, res) => {
-        const psicologo = await psicologosModel.findAll();
-        return res.json(psicologo);
-    },
-    listOne: async (req, res) => {
-        const { id_psicologo } = req.params;
-        const onePsicologo = await psicologosModel.findByPk(id_psicologo);
-        res.json(onePsicologo);
-    },
-    createPsicologo: async (req, res) => {
-        const { nome, email,senha, apresentacao } = req.body;
-        const newPsicologo = await psicologosModel.create({
-            nome, 
-            email, 
-            senha,
-            apresentacao,
-        });
-        res.json(newPsicologo);
-    },
-    deleteOne: async (req, res) => {
-        const { id_psicologo } = req.params;
-        await psicologosModel.destroy({
-            where:{
-                id_psicologo,
-            },    
-        });
-        res.json("ID deletado com sucesso");
-    },
-    editOne: async(req, res) => {
-        const { id_psicologo } = req.params;
-        const { nome, email, senha, apresentacao } = req.body;
-        const psicologoUpdated = await psicologosModel.update(
-            {
-                nome, email, senha, apresentacao 
-            },
-            {
-                where: {
-                    id_psicologo,
-                },
+            try {
+                const psicologo = await psicologosModel.findAll();
+
+                return res.status(200).json(psicologo);
+                
+            } catch (error) {
+                res.json("Não foi possível exibir a lista!");
+                console.error(error);              
             }
-        );
-        res.json("Cadastro Atualizado");
     },
+
+    listOne: async (req, res) => {
+
+        try {
+
+            const { id_psicologo } = req.params;
+            const onePsicologo = await psicologosModel.findByPk(id_psicologo);
+            if(!onePsicologo) {
+                res.status(404).json("Id não encontrado");
+            }
+            res.status(200).json(onePsicologo);
+            
+        } catch (error) {
+            console.error(error);         
+        }
+    },
+
+    createPsicologo: async (req, res) => {
+        try {
+            const { nome, email,senha, apresentacao } = req.body;
+
+            const newPsicologo = await psicologosModel.create({
+                nome, 
+                email, 
+                senha,
+                apresentacao,
+            });
+
+             res.status(201).json(newPsicologo);
+            
+        } catch (error) {
+            console.error(error);
+            
+        }
+        
+    },
+
+    deleteOne: async (req, res) => {
+        try {
+            const { id_psicologo } = req.params;
+            const psicologoID = await psicologosModel.findByPk(id_psicologo);
+
+            await psicologosModel.destroy({
+                where:{
+                    id_psicologo,
+                },    
+            });
+
+            if (!psicologoID) {
+                return res.status(404).json("Id não encontrado");} 
+    
+                res.status(204).json("ID deletado com sucesso");
+            
+        } catch (error){
+            console.error(error);
+         
+        }
+    },
+
+
+    editOne: async(req, res) => {
+        try {
+            const { id_psicologo } = req.params;
+            const { nome, email, senha, apresentacao } = req.body;
+
+
+            const psicologoUpdated = await psicologosModel.update(
+                {
+                    nome, email, senha, apresentacao 
+                },
+                {
+                    where: {
+                        id_psicologo,
+                    },
+                }
+            );
+    
+            res.status(200).json("Atualizado");
+            
+        } catch {
+            res.status(400);
+            
+        }
+
+       
+    },   
 };
+
 module.exports = psicologosController;
